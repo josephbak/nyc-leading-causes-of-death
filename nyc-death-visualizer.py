09742cea95b42851b7e07027570e7345625f51fd
Joseph Bak <joseph.bak31415@gmail.com>
2022-12-02 13:57:54 -0500

    user selected dataframe added

--- nyc-death-visualizer.py
+++ nyc-death-visualizer.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+import plotly.express as px
+
 
 # function to pre-process the data
 def load_data():
@@ -32,11 +34,12 @@ def load_data():
     df = df.drop(columns = ['Sex', 'Race Ethnicity'])
     return df
 
-df = pd.read_csv('modified_nyc_deaths_final.csv')
+# df = pd.read_csv('modified_nyc_deaths_final.csv')
+df = load_data()
 
 st.title('NYC Leading Causes of Death')
 st.markdown("""
-This is and interactive app between leading causes of death and race/sex in NYC.
+This is an interactive app between leading causes of death and race/sex in NYC.
 """)
 
 # upleaded_file = st.file_uploader('Upload your file here')
@@ -47,16 +50,55 @@ st.sidebar.header('User Input Features')
 # selected_years = st.sidebar.selectbox('Year', sorted(list(df['Year'].unique())))
 
 sorted_year_unique = sorted(df['Year'].unique()) 
-selected_years = st.sidebar.multiselect('Year',sorted_year_unique) 
+selected_years = st.sidebar.multiselect('Year',sorted_year_unique, sorted_year_unique) 
 
 sorted_leading_cause_unique = sorted(df['Leading Cause'].unique())
-selected_leading_cause= st.sidebar.multiselect('Leading Causes of Death', sorted_leading_cause_unique)
+selected_leading_cause= st.sidebar.multiselect('Leading Causes of Death', sorted_leading_cause_unique, sorted_leading_cause_unique)
 
 sorted_race_and_sex_unique = sorted(df['Race Ethnicity Sex'].unique())
-selected_race_and_sex = st.sidebar.multiselect('Race and Sex', sorted_race_and_sex_unique)
+selected_race_and_sex = st.sidebar.multiselect('Race and Sex', sorted_race_and_sex_unique, sorted_race_and_sex_unique)
 
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=["a", "b", "c"])
+# filtering data
+df_selected_data = df[(df['Year'].isin(selected_years)) & (df['Leading Cause'].isin(selected_leading_cause)) & (df['Race Ethnicity Sex'].isin(selected_race_and_sex))]
+# display the selected data frame
+st.header('Display selected data')
+st.write('Data Dimension: ' + str(df_selected_data.shape[0]) + ' rows and ' + str(df_selected_data.shape[1]) + ' columns.')
+st.dataframe(df_selected_data)
 
-st.bar_chart(chart_data)
+
+# chart_data = pd.DataFrame(
+#     np.random.randn(20, 3),
+#     columns=["a", "b", "c"])
+# st.bar_chart(chart_data)
+
+# def interactive_plot_example():
+#     x_axis_val = st.selectbox('Select the X-axis', options=df.columns)
+#     y_axis_val = st.selectbox('Select the Y-axis', options=df.columns)
+
+#     plot = px.scatter(df, x=x_axis_val, y=y_axis_val)
+#     st.plotly_chart(plot, use_container_width=True)
+
+# interactive_plot_example()
+
+# st.write(selected_years)
+# st.write(type(selected_years))
+
+# def interactive_plot(years, causes, race_and_sex):
+#     fig = px.bar(df, )
+#     return
+
+
+def interactive_plot(df):
+    # fig = px.bar(df, x="Deaths", y="Leading Cause", color="Race Ethnicity Sex", orientation='h')
+    # fig.show()
+    plot = px.bar(df, x="Deaths", y="Leading Cause", color="Race Ethnicity Sex", orientation='h')
+    st.plotly_chart(plot)
+
+
+# st.bar_chart(df, x = selected_race_and_sex, y = selected_leading_cause)
+
+interactive_plot(df_selected_data)
+
+
+# data frame extractor function from user input features
+# draw horizontal bar graph using user chosen data frame
